4b79c20f2ad1edbd5d436a373776a12b8a99eb41
Anuj Sharma <anuj_sharma401@yahoo.com>
2017-09-01 00:35:52 +0530

    Update package.rb

--- cookbooks/erlang/recipes/package.rb
+++ cookbooks/erlang/recipes/package.rb
@@ -46,6 +46,7 @@ when 'rhel', 'suse', 'fedora', 'amazon', 'centos'
   end
 
   include_recipe 'yum-epel' if node['platform_family'] == 'rhel' && node['erlang']['package']['install_epel_repository']
+  include_recipe 'yum-epel' if node['platform_family'] == 'amazon' && node['erlang']['package']['install_epel_repository']
   package 'erlang' do
     version node['erlang']['package']['version'] if node['erlang']['package']['version']
   end
